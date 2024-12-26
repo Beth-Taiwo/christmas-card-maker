d3259b0d05e0e6f9f82ca82a7e030d45623a7807
Beth Taiwo <popoolataiwo22@gmail.com>
2024-12-26 11:22:14 +0000

    spaced elements

--- components/ChristmasCard.vue
+++ components/ChristmasCard.vue
@@ -3,7 +3,9 @@
     class="w-full bg-red-500 h-full flex flex-col text-white text-7xl font-bold p-10 items-center justify-center"
     style="background-image: url(/img/sincerely-media-8EhZobADF8M-unsplash.jpg)"
   >
-    <div class="font-bold mb-8" style="font-size: 100px">{{ greeting }}</div>
+    <div class="font-bold mb-8 text-center" style="font-size: 100px">
+      {{ greeting }}
+    </div>
     <img
       v-if="photo"
       :src="photo"
@@ -11,7 +13,7 @@
       style="width: 400px; height: 400px"
     />
     <div class="bg-red-500 px-2 py-2">From</div>
-    <div>{{ name || "Add your name here" }}</div>
+    <div class="mt-8">{{ name || "Add your name here" }}</div>
   </div>
 </template>
 
